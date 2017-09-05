ad9d5a08b06de4cde6bef698dd8ce57546268ddb
Alex Beeken <aeb242@gmail.com>
2017-09-04 18:39:49 -0700

    Update router.ex

--- lib/mrbeeken_backend_web/router.ex
+++ lib/mrbeeken_backend_web/router.ex
@@ -1,14 +1,6 @@
 defmodule MrbeekenBackendWeb.Router do
   use MrbeekenBackendWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug CORSPlug, [origin: "http://localhost:4200"]
     plug :accepts, ["json", "json-api"]
@@ -16,20 +8,9 @@ defmodule MrbeekenBackendWeb.Router do
     plug JaSerializer.Deserializer
   end
 
-  scope "/", MrbeekenBackendWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
   scope "/api/v1", MrbeekenBackendWeb do
     pipe_through :api
 
     get "/users", UserController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", MrbeekenBackendWeb do
-  #   pipe_through :api
-  # end
 end
