e8947438a6d97be9009aa1c7840b0bfb98c1a899
Alex Beeken <aeb242@gmail.com>
2017-09-07 09:34:24 -0700

    check for matching password

--- lib/mrbeeken_backend_web/controllers/sessions_controller.ex
+++ lib/mrbeeken_backend_web/controllers/sessions_controller.ex
@@ -6,17 +6,22 @@ defmodule MrbeekenBackendWeb.SessionsController do
 
   def create(conn, params) do
     user = Repo.get_by(User, email: params["username"]) |> Repo.preload(:session)
-    correct_pw = Comeonin.Bcrypt.checkpw(params["password"], user.password_hash)
-    changeset = Session.changeset(%Session{}, %{user_id: user.id})
-    case Repo.insert(changeset) do
-     {:ok, session} ->
-       conn
-       |> put_status(201)
-       |> render("show.json-api", data: session)
-     {:error, changeset} ->
-       conn
-       |> put_status(422)
-       |> render(:errors, data: changeset)
-   end
+    unless Comeonin.Bcrypt.checkpw(params["password"], user.password_hash) do
+      conn
+      |> put_status(400)
+      |> render(:errors, data: %{errors: [%{message: "bad_password"}]})
+    else
+      changeset = Session.changeset(%Session{}, %{user_id: user.id})
+      case Repo.insert(changeset) do
+       {:ok, session} ->
+         conn
+         |> put_status(201)
+         |> render("show.json-api", data: session)
+       {:error, changeset} ->
+         conn
+         |> put_status(400)
+         |> render(:errors, data: changeset)
+      end
+    end
   end
 end
