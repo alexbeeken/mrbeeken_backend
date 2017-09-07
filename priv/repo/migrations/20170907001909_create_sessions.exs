8c626c3ee90b93e10dbc91a2bc1722af833e4244
Alex Beeken <aeb242@gmail.com>
2017-09-07 09:55:11 -0700

    fix table name typo

--- priv/repo/migrations/20170907001909_create_sessions.exs
+++ priv/repo/migrations/20170907001909_create_sessions.exs
@@ -2,7 +2,7 @@ defmodule MrbeekenBackend.Repo.Migrations.CreateSessions do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:sessions) do
       add :token, :string
       add :user_id, references(:users)
 
