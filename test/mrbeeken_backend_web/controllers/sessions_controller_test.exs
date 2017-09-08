a81093a0982cad0534b5ab0eaeedad054ff37e97
Alex Beeken <aeb242@gmail.com>
2017-09-07 21:56:25 -0700

    put headers in setup

--- test/mrbeeken_backend_web/controllers/sessions_controller_test.exs
+++ test/mrbeeken_backend_web/controllers/sessions_controller_test.exs
@@ -7,11 +7,14 @@ defmodule MrbeekenBackendWeb.SessionsControllerTest do
   @request_attrs %{username: "test@example.com", password: "123456abc"}
   @user_attrs %{email: "test@example.com", password: "123456abc", password_confirmation: "123456abc"}
 
-  test "#create successfully returns a session object" do
+  setup do
     conn = build_conn()
       |> put_req_header("accept", "application/vnd.api+json")
       |> put_req_header("content-type", "application/vnd.api+json")
+      {:ok, conn: conn}
+  end
 
+  test "#create successfully returns a session object", %{conn: conn} do
     user = %User{}
             |> User.changeset(@user_attrs)
             |> Repo.insert!
