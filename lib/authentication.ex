afa43dd5420adfb58371c11cb797762fc7022bfd
Alex Beeken <aeb242@gmail.com>
2017-09-13 14:55:42 -0700

    render error in authentication plug instead of dummy controller

--- lib/authentication.ex
+++ lib/authentication.ex
@@ -1,8 +1,9 @@
 defmodule MrbeekenBackendWeb.Authentication do
   import Plug.Conn
+  import Phoenix.Controller
 
   alias MrbeekenBackend.{Repo}
-  alias MrbeekenBackendWeb.{User}
+  alias MrbeekenBackendWeb.{User, ErrorView, Errors}
 
   def init(opts) do
     opts
@@ -10,26 +11,41 @@ defmodule MrbeekenBackendWeb.Authentication do
 
   def call(conn, opts) do
     token = find_token(conn.req_headers)
-    "Bearer " <> token = token
-    case Guardian.decode_and_verify(token) do
-      {:ok, claims} ->
-        "User:" <> user_id = claims["sub"]
-        user = User |> Repo.get(user_id)
-        conn
-        |> assign(:current_user, user)
-      {:error, _claims} ->
-        conn
+    unless token do
+      render_error(conn)
+    else
+      "Bearer " <> token = token
+      case Guardian.decode_and_verify(token) do
+        {:error, _claims} ->
+          render_error(conn)
+        {:ok, claims} ->
+          "User:" <> user_id = claims["sub"]
+          user = User |> Repo.get(user_id)
+          conn
+          |> assign(:current_user, user)
+      end
     end
   end
 
-  def find_token([ h | t ]) do
+  def find_token([ h | t ] \\ [ nil | nil]) do
     case h do
       {"authorization", x} ->
         x
       {_, _} ->
-        find_token(t)
+        if t == [] do
+          nil
+        else
+          find_token(t)
+        end
       _ ->
         nil
     end
+  end
+
+  def render_error(conn) do
+    conn
+    |> put_status(400)
+    |> render(ErrorView, "400.json-api", %{title: Errors.session_bad})
+    |> halt()
   end
 end
--- test/mrbeeken_backend_web/controllers/users_controller_test.exs
+++ test/mrbeeken_backend_web/controllers/users_controller_test.exs
@@ -2,15 +2,14 @@ defmodule MrbeekenBackendWeb.UsersControllerTest do
   use MrbeekenBackendWeb.ConnCase
   alias MrbeekenBackend.Repo
   alias MrbeekenBackendWeb.{User, UserView}
-  import JsonApi
-
-  @request_attrs %{username: "test@example.com", password: "123456abc"}
-  @user_attrs %{email: "test@example.com", password: "123456abc", password_confirmation: "123456abc"}
+  import MrbeekenBackendWeb.{JsonApi, Factory, LoginHelper}
 
   setup do
+    user = insert(:user)
     conn = build_conn()
       |> json_api_headers
-    {:ok, conn: conn}
+      |> login(user)
+    {:ok, conn: conn, user: user}
   end
 
   def render_json(template, assigns) do
@@ -19,10 +18,9 @@ defmodule MrbeekenBackendWeb.UsersControllerTest do
     UserView.render(template, assigns) |> encode
   end
 
-  test "#show returns a user object", %{conn: conn} do
-    # user = %User{} |> User.changeset(@user_attrs) |> Repo.insert!
-    # conn = post conn, users_path(conn, :show), @valid_attrs
-    #
-    # assert json_response(conn, 201) == render_json("show.json-api", %{user: user})
+  test "#show returns a user object", %{conn: conn, user: user} do
+    conn = get conn, user_path(conn, :show, user.id)
+
+    assert json_response(conn, 200) == render_json("show.json-api", %{user: user})
   end
 end
