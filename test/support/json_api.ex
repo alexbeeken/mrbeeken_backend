6bd53ceaa6e8edb282d152cd2614e02a1a956783
Alex Beeken <aeb242@gmail.com>
2017-09-16 15:31:10 -0700

    Update json_api.ex

--- test/support/json_api.ex
+++ test/support/json_api.ex
@@ -5,18 +5,18 @@ defmodule MrbeekenBackendWeb.JsonApi do
 
   def json_api_headers(conn) do
     conn
-      |> put_req_header("accept", "application/vnd.api+json")
-      |> put_req_header("content-type", "application/vnd.api+json")
+    |> put_req_header("accept", "application/vnd.api+json")
+    |> put_req_header("content-type", "application/vnd.api+json")
   end
 
   def valid_token(conn) do
     conn
-      |> put_req_header("authorization", TestCreds.valid_token)
+    |> put_req_header("authorization", TestCreds.valid_token)
   end
 
   def invalid_token(conn) do
     conn
-      |> put_req_header("authorization", TestCreds.invalid_token)
+    |> put_req_header("authorization", TestCreds.invalid_token)
   end
 
   def render_error(template, assigns) do
