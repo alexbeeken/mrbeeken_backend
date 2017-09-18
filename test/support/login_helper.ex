da2af0781817a94ee7d52803b4ad2428a23a68d6
Alex Beeken <aeb242@gmail.com>
2017-09-17 19:53:35 -0700

    fix small style issue

--- test/support/login_helper.ex
+++ test/support/login_helper.ex
@@ -1,12 +1,11 @@
 defmodule MrbeekenBackendWeb.LoginHelper do
   use Phoenix.ConnTest
-  
+
   alias MrbeekenBackend.Repo
   alias MrbeekenBackendWeb.User
 
   def login(conn, user) do
     {:ok, jwt, _} = Guardian.encode_and_sign(user)
-    conn
-    |> put_req_header("authorization", "Bearer #{jwt}")
+    put_req_header(conn, "authorization", "Bearer #{jwt}")
   end
 end
